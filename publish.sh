2fe54aa970d0518ca858cf0247898b9e78f5ec29
georemo <george.oremo@gmail.com>
2024-03-08 20:19:14 +0300

    set version v0.0.2

--- publish.sh
+++ publish.sh
@@ -2,11 +2,11 @@
 echo "current repository latest version:\n"
 git ls-remote --tags https://github.com/tcp-x/cd-plug-util.git
 # set latest version
-Version="v0.0.1"
+Version="v0.0.2"
 
 # cd $projDir
 go mod tidy
-git add user.go
+git add go.mod i-exec.go
 git commit -am "set version $Version"
 git tag $Version
 git push origin $Version
