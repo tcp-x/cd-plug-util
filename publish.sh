4929df0f475f5a8971761763375017534eed5399
georemo <george.oremo@gmail.com>
2024-03-09 20:00:33 +0300

    set version v0.0.13

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 echo "current repository latest version:\n"
 git ls-remote --tags https://github.com/tcp-x/cd-plug-util.git
 # set latest version
-Version="v0.0.12"
+Version="v0.0.13"
 
 # cd $projDir
 go mod tidy
